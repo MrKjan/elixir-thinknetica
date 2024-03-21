e6a8a96aed52c9c5d0fd79d43e6e1d565962b0c7
Nikolai Ekkore <ekkore.nick@yandex.ru>
2024-03-21 04:49:51 +0300

    fix issues, done genserver implementation

--- pingpong/test/pingpong_test.exs
+++ pingpong/test/pingpong_test.exs
@@ -5,13 +5,7 @@ defmodule PingpongTest do
   test "server pongs" do
     server_pid = spawn(Pingpong, :server, [])
     send(server_pid, {:ping, self()})
-    receive do
-      response -> assert({:pong, _node} = response)
-    after
-      # obviously its a clutch
-      1_000 ->
-        assert(false, "test time limit exceeded")
-    end
+    assert_receive({:pong, _node})
     Process.exit(server_pid, :time_to_die)
   end
 end
